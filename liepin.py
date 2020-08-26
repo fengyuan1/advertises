f54ecaa50b667bf770ba5f4a94a4446de6912e0f
fengyuan@ifreegroup.com <939231156@qq.com>
2020-08-26 10:59:51 +0800

    ceshi

--- liepin.py
+++ liepin.py
@@ -4,12 +4,11 @@ import csv
 import time
 import requests
 from urllib.parse import quote
-import random
 
 class Main:
 	def index(self,i):
 		# 岗位
-		work_name="java"
+		work_name="解决方案架构师"
 		link="https://www.liepin.com/zhaopin/?industries=040&subIndustry=&dqs=050020&salary=&jobKind=&pubTime=&compkind=&compscale=&searchType=1&isAnalysis=&sortFlag=15&d_headId=aaa42964a7680110daf82f6e378267d9&d_ckId=ff5c36a41d1d524cff2692be11bbe61f&d_sfrom=search_prime&d_pageSize=40&siTag=_1WzlG2kKhjWAm3Yf9qrog%7EqdZCMSZU_dxu38HB-h7GFA&key="+quote(work_name)+"&d_curPage="+str(i);
 		headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:47.0) Gecko/20100101 Firefox/47.0"}
 
@@ -17,27 +16,28 @@ class Main:
 		response.encoding = 'utf-8'
 		html = response.text
 		soup=BeautifulSoup(html,'html.parser')
-		# print(soup)
 		sojob_result=soup.find("div",class_='sojob-result')
 		list=sojob_result.find_all("li")
 		# 岗位
+		try:
+			for x in range(0,len(list)):
+				address = list[x].find("a", class_='area').get_text().strip()
+				work=list[x].find("a").get_text().strip()
+				edu=list[x].find("span",class_='edu').get_text().strip()
+				year=list[x].find("span",class_='edu').find_next_sibling("span").get_text().strip()
+				money=list[x].find("span",class_='text-warning').get_text().strip()
+				company=list[x].find("p",class_='company-name').get_text().strip()
 
-		for x in range(0,len(list)):
-			work=list[x].find("a").get_text().strip()
-			edu=list[x].find("span",class_='edu').get_text().strip()
-			year=list[x].find("span",class_='edu').find_next_sibling("span").get_text().strip()
-			money=list[x].find("span",class_='text-warning').get_text().strip()
-			company=list[x].find("p",class_='company-name').get_text().strip()
-			self.write([work,edu,money,company,year])
-			print([work,edu,money,company,year])
-
+				self.write([work,edu,money,company,year,address])
+				print([work,edu,money,company,year,address])
+		except:
+			Main().index(i+1)
 
 	def write(self,data):
 		with open('data/lieping'+time.strftime("%Y-%m-%d", time.localtime())+'.csv', 'a+', encoding='utf-8', newline='') as f:
 			writer = csv.writer(f)
 			writer.writerow(data)
-			print(data)
+			print('写入成功')
 
 for i in range(0,200):
 	Main().index(i);
-	time.sleep(random.randint(0, 5))
--- qiancheng.py
+++ qiancheng.py
@@ -10,14 +10,16 @@ class Main:
         work_name="运营总监"
         link="https://search.51job.com/list/030200,000000,0000,00,9,99,"+quote(work_name)+",2,"+str(i)+".html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
-
+        #print(link)
         response = requests.get(link, headers=headers)
         code=response.apparent_encoding
         response.encoding = code
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
-        sojob_result = soup.find("div", id='resultList')
-        list = sojob_result.find_all("div" , class_='el')
+       
+        sojob_result = soup.find("script", type='text/javascript')
+        print(sojob_result)
+        list = sojob_result.find_all("div" , class_='e')
         for x in range(1,len(list)):
             # print(list[x])
             work=list[x].find("p",class_='t1').find("span").get_text().strip()
@@ -34,7 +36,7 @@ class Main:
             writer.writerow(data)
             print(data)
 
-for i in range(0,200):
+for i in range(1,200):
     Main().index(i)
 
 print('恭喜你，爬取数据成功')
